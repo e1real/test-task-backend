675ed053f17080773817cba9c113494943a56ad1
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-07-22 02:55:46 +0600

    feat(deployment): configure testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,38 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
+use Faker\Generator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    protected Generator $faker;
+
+    /**
+     * Sets up the tests
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Faker::create();
+
+        Artisan::call('migrate');
+    }
+
+
+    /**
+     * Rolls back migrations
+     */
+    public function tearDown(): void
+    {
+        Artisan::call('migrate:rollback');
+
+        parent::tearDown();
+    }
 }
